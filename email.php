0c14057dcc7ec1e2d45733813aef2a9a12aee5f3
DeLaneyBJones <40803844+DeLaneyBJones@users.noreply.github.com>
2019-03-11 14:03:03 -0700

    EmailFunctioning

--- email.php
+++ email.php
@@ -1,39 +1,84 @@
 <?php
-    function BadEmail($str)
-    {
-        $injections = array('(\n+)'),
-        '(\r+)', '(\t+)', '(%0A+)', '(%0D+)', '(%08+)', '(%09+)');
+/*
+ *  CONFIGURE EVERYTHING HERE
+ */
+$from = $_POST['email'];
 
-        $inject = join("|", $injections);
-        $inject = "/$inject/i";
+$message = $_POST['message'];
 
-        if(match($inject, $str))
-        {
-            return true;
+echo($inputEmail);
+echo($message);
+
+// an email address that will be in the From field of the email.
+//$from = 'Demo contact form <demo@domain.com>';
+
+// an email address that will receive the email with the output of the form
+//$sendTo = 'Demo contact form <demo@domain.com>';
+$sendTo = "dbjones2518@lcmail.lcsc.edu";
+
+// subject of the email
+//$subject = 'New message from contact form';
+$subject = $message;
+
+// form field names and their translations.
+// array variable name => Text to appear in the email
+//$fields = array('name' => 'Name', 'surname' => 'Surname', 'need' => 'Need', 'email' => 'Email', 'message' => 'Message'); 
+
+// message that will be displayed when everything is OK :)
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error while submitting the form. Please try again later';
+
+/*
+ *  LET'S DO THE SENDING
+ */
+
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
+
+try
+{
+
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
+            
+    $emailText = "You have a new message from your contact form\n=============================\n";
+
+    /*foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email 
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
         }
-        else{
-            return false;
-        }
-    }
+    }*/
+    $emailText = $message;
 
-    if(IsInjected($visitor_email))
-    {
-        echo "Bad email value!";
-        exit;
-    }
+    // All the neccessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
+    
+    // Send email
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
 
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
 
-    if ($visitor_email == "" || $message == "")
-    {
-        echo "You must fill in all feilds";
-        exit;
-    }
 
-    $to = "dbjones2518@lcmail.lcsc.edu";
-    $headers = "From: $visitor_email \r\n";
-    mail($to, $email_subject, $message, $headers);
-?>
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
 
-<!--http://form.guide/email-form/php-form-to-email.html-->
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
